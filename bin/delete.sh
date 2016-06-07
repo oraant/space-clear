31f082c2cc2c141ab6e5b8cadd3d90853a1e3e27
oraant <oraant777@gmail.com>
2016-06-07 21:38:53 +0800

    updating delete.sh,and we can configure files in black.conf.

--- bin/delete.sh
+++ bin/delete.sh
@@ -18,23 +18,62 @@ check_user
 
 # init environment
 #export PATH=$PATH:/usr/bin:/usr/sbin:/sbin:/bin:.
-tmp_disk_stat=$SC_HOME/tmp/disk.tmp
-tmp_calc_conf=$SC_HOME/tmp/disk.cfg
-tmp_lock_file=$SC_HOME/tmp/disk.lck
+#  tmp_disk_stat=$SC_HOME/tmp/disk.tmp
+#  tmp_calc_conf=$SC_HOME/tmp/disk.cfg
+#  tmp_lock_file=$SC_HOME/tmp/disk.lck
 
-# get disk configurations
+# verify black.cfg
 function verify_black_config()
 {
     cat $black_cfg |egrep -v '^$|^#' |while read directory
     do
-        if [ -d $directory ] || [ -f $directory ]
+        if [ ! -d $directory ] && [ ! -f $directory ]
         then
-            echo -n ''
-        else
-            error "Disk Configure file can not parse."
-            error "Please check the ${part} partition in the configure file: ${disk_cfg}"
+            error "${directory} in Black Configure file can not parse."
+            error "Please check the ${directory} in the configure file: ${black_cfg}"
             exit 5
         fi
     done
 }
+
+# verify delete.cfg 
+function verify_delete_config()
+{
+    cat $delete_cfg |egrep -v '^$|^#' |while read time act files
+    do
+        if [[ ! $time =~ ^[0-9]+[m|h|d]$ ]]
+        then
+            error "${time} in Delete Configure file can not parse."
+            error "Please check the time of ${files} in the configure file: ${delete_cfg}"
+            exit 5
+        fi
+
+        if [[ $act != 'delete' ]] && [[ $act != 'clear' ]]
+        then
+            error "${act} in Delete Configure file can not parse."
+            error "Please check the action of ${files} in the configure file: ${delete_cfg}"
+            exit 5
+        fi
+
+        ls $files &>/dev/null
+        if [[ $? != 0 ]]
+        then
+            error "${files} in Delete Configure file can not parse."
+            error "Please check the files pattern of ${files} in the configure file: ${delete_cfg}"
+            exit 5
+        fi
+    done
+}
+
+# filter files and delete it.
+function filter_and_delete()
+{
+    cat $delete_cfg |egrep -v '^$|^#' |while read time act files
+    do
+        /bin/pwd
+    done
+}
+
 verify_black_config
+verify_delete_config
+filter_and_delete
