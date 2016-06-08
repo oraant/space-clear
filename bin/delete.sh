504fba97ba9ce1435370a74ce14d39a95ac998f0
oraant <oraant777@gmail.com>
2016-06-08 15:36:54 +0800

    complete delete.sh.

--- bin/delete.sh
+++ bin/delete.sh
@@ -21,6 +21,12 @@ check_user
 tmp_remove_cfg=$SC_HOME/tmp/delete.cfg
 tmp_clear_cfg=$SC_HOME/tmp/clear.cfg
 
+total_files=0
+removed_files=0
+cleared_files=0
+blacked_files=0
+too_new_files=0
+
 # verify black.cfg
 function verify_black_config()
 {
@@ -29,7 +35,7 @@ function verify_black_config()
         if [ ! -d $directory ]
         then
             error "${directory} in Black Configure file can not parse."
-            error "Please check the ${directory} in the configure file: ${black_cfg}"
+            error "Please check the ${directory} in the configure file: \$SC_HOME/conf/black.cfg"
             exit 5
         fi
     done
@@ -43,14 +49,14 @@ function verify_delete_config()
         if [[ ! $time =~ ^[0-9]+[m|h|d]$ ]]
         then
             error "${time} in Delete Configure file can not parse."
-            error "Please check the time of ${files} in the configure file: ${delete_cfg}"
+            error "Please check the time of ${files} in the configure file: \$SC_HOME/conf/delete.cfg"
             exit 5
         fi
 
         if [[ $act != 'remove' ]] && [[ $act != 'clear' ]]
         then
             error "${act} in Delete Configure file can not parse."
-            error "Please check the action of ${files} in the configure file: ${delete_cfg}"
+            error "Please check the action of ${files} in the configure file: \$SC_HOME/conf/delete.cfg"
             exit 5
         fi
 
@@ -58,36 +64,79 @@ function verify_delete_config()
         if [[ $? != 0 ]]
         then
             error "${files} in Delete Configure file can not parse."
-            error "Please check the files pattern of ${files} in the configure file: ${delete_cfg}"
+            error "Please check the files pattern of ${files} in the configure file: \$SC_HOME/conf/delete.cfg"
             exit 5
         fi
     done
 }
 
+function time_to_second()
+{
+    time=$1
+    unit=${time: -1}
+    value=$(echo $time|tr -d 'mhd')
+    case $unit in
+        'm')echo $(( value*60 ));;
+        'h')echo $(( value*60*60 ));;
+        'd')echo $(( value*60*60*24 ));;
+    esac
+}
+
+function file_mtime_gap()
+{
+    mtime=$(stat --printf=%Y $1)
+    ntime=$(date +%s)
+    echo $ntime-$mtime|bc
+}
+
 # filter files and delete it.
 function filter_configure_file()
 {
-    cat $delete_cfg |egrep -v '^$|^#' |while read time act files
+    dirs=($(cat $black_cfg |egrep -v '^$|^#'))
+    while read time act files
     do
-        cat $black_cfg |egrep -v '^$|^#' |while read directory
+        for file in $files #in case of `ll dir*/file*.sh`
         do
-            for file in $files #in case of `ll dir*/file*.sh`
-            do
-                filepath=$(dirname $(readlink -f $file))
-                echo $filepath $directory
-                if [[ $filepath == $directory ]];then
-                    echo 'match'
-                    debug "$file has filtered by $directory in the configure file: $black_cfg."
-                elif [[ $act == 'delete' ]];then
-                    echo "$time $file">>$tmp_remove_cfg
-                    debug "$file will be removed"
-                elif [[ $act == 'clear' ]];then
-                    echo "$time $file">>$tmp_clear_cfg
-                    debug "$file will be cleared"
-                fi
-            done
+            total_files=$(( total_files+1 ))
+            file_gap=$(file_mtime_gap $file)
+            conf_gap=$(time_to_second $time)
+            if [[ $conf_gap -gt $file_gap ]];then
+                too_new_files=$(( too_new_files+1 ))
+                debug "the mtime gap of $file is $file_gap seconds,less than $time configured in: \$SC_HOME/conf/delete.cfg"
+                continue
+            fi
+
+            filepath=$(dirname $(readlink -f $file))
+
+            if [[ " ${dirs[@]} " =~ " $filepath " ]];then
+                blacked_files=$(( blacked_files+1 ))
+                debug "$file has been filtered by $filepath in the configure file: \$SC_HOME/conf/black.cfg"
+            elif [[ $act == 'remove' ]];then
+                removed_files=$(( removed_files+1 ))
+                echo $file>>$tmp_remove_cfg
+                debug "$file will be removed"
+            elif [[ $act == 'clear' ]];then
+                cleared_files=$(( cleared_files+1 ))
+                echo $file>>$tmp_clear_cfg
+                debug "$file will be cleared"
+            fi
         done
-    done
+    done < <(cat $delete_cfg |egrep -v '^$|^#')
+    log "Total: $total_files\tToo New: $too_new_files\tBlacklist: $blacked_files\tRemoved: $removed_files\tCleared: $cleared_files"
+}
+
+function delete_files()
+{
+    if [ -f $tmp_remove_cfg ];then
+        tmp_path_r=($(cat $tmp_remove_cfg))
+        echo "rm ${tmp_path_r[@]}"
+    fi
+
+    if [ -f $tmp_clear_cfg ];then
+        for tmp_path_c in $(<$tmp_clear_cfg);do
+            echo "> $tmp_path_c"
+        done
+    fi
 }
 
 function clear_tmp_file()
@@ -96,7 +145,13 @@ function clear_tmp_file()
     [ -f $tmp_clear_cfg ] && rm $tmp_clear_cfg
 }
 
-verify_black_config
-verify_delete_config
-filter_configure_file
-#clear_tmp_file
+function main()
+{
+    verify_black_config \
+    && verify_delete_config \
+    && filter_configure_file \
+    && delete_files
+
+    clear_tmp_file
+}
+main
